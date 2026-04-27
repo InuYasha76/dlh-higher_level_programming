22b8b02bf0699201f22039e8169cdda0233700bd
gcanavag <gcanavag@student.42luxembourg.lu>
2026-04-27 19:51:15 +0200

    Fix typoe max->max_val

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     max_val = my_list[0]
-    for val in my_list:
+    for max_val in my_list:
         if max_val < val:
             max_val = val
-    return max
+    return max_val
