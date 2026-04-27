8c56f68f2ffa92ccfb9b07c23235a3c06e540adb
gcanavag <gcanavag@student.42luxembourg.lu>
2026-04-27 19:48:11 +0200

    Commit#1

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    val_max = my_list[0]
+    for val in my_list:
+        if max < val:
+            max = val
+    return max
