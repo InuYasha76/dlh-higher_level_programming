39af503df552e4ff86acc5840562f3ef8696a64a
gcanavag <gcanavag@student.42luxembourg.lu>
2026-04-29 13:11:42 +0200

    correct misstype dict.value() -> dict.get()

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -2,6 +2,6 @@
 def complex_delete(a_dictionary, value):
     if a_dictionary:
         for k in list(a_dictionary.keys()):
-            if a_dictionary.value(k) == value:
+            if a_dictionary.get(k) == value:
                 a_dictionary.pop(k, None)
     return a_dictionary
