52ede1bda3dc9e2b7bfd9eb767269ca9372ddcd7
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-05 13:12:42 +0200

    Add Docstrings

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+"""This Module defines a Square class"""
+
+
 class Square:
+    """
+    A class that represents a square.
+
+    Attributes:
+        size (int): The length of a side of a square.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+        size (int): The size of the new square.
+        """
         self.__size = size
