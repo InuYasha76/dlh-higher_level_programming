b5ce1ee4a108beac8ff06324a880888d08bf8861
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-05 16:10:43 +0200

    Docstrings for my_print

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,14 +1,53 @@
 #!/usr/bin/python3
+"""This Module defines a Square class"""
+
+
 class Square:
+    """
+    A class that represents a square.
+
+    This class checks the side length of a square,
+    called "size", ensuring it is a positive integer
+    and providing a method to calculate the area.
+
+    """
+
     def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+        size (int): The length of a side of a square.
+                    Defaults to 0 if not provided.
+
+        The direct assignment has been replaced
+        by a call to the setter to verify that
+        size is a positive integer.
+        """
         self.size = size
 
     @property
     def size(self):
+        """
+        Gets the size of the square.
+
+        Returns:
+            int: The side length of the square.
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Sets the size of the square with validation.
+
+        Args:
+            value (int): The new size to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -17,9 +56,21 @@ class Square:
             self.__size = value
 
     def area(self):
+        """
+        Calculates the current square area.
+
+        Returns:
+            int: The area of the square (size squared).
+        """
         return self.__size**2
 
     def my_print(self):
+        """
+        Prints a square to stdout.
+
+        The square is made of  __size x __sizea '#' characters
+        If `__size` is 0, only an empty line is printed.
+        """
         if not self.__size:
             print("")
         else:
