81bcb15e5766fe8879e4e98b0f3ed8247586a5ea
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-07 17:05:29 +0200

    Fix PEP8 error:

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -7,6 +7,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, attrs=None):
         if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
             return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
