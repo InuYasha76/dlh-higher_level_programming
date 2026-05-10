4658d83af07fd967aa16a4e0326a40c56e8a9212
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-10 22:21:59 +0200

    PEP8

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -4,11 +4,13 @@
 
 import sys
 
+
 def print_stats(occurrences, total_size):
     print(f"File size: {total_size}")
     for code in sorted(occurrences.keys()):
         if occurrences[code] > 0:
             print(f"{code}: {occurrences[code]}")
+
 
 def run_stats():
     http_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
@@ -36,6 +38,7 @@ def run_stats():
 
     finally:
         print_stats(occurrences, total_size)
+
 
 if __name__ == "__main__":
     run_stats()
