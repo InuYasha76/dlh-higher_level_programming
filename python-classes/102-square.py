86b790f7662899122974cbb931f19e1e054f607f
gcanavag <gcanavag@student.42luxembourg.lu>
2026-05-10 19:08:17 +0200

    Add task 102

---
+++ python-classes/102-square.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+"""This Module defines a Square class"""
+
+
+class Square:
+    """
+    A class that represents a square.
+
+    This class checks the side length of a square,
+    called "size", ensuring it is a positive integer
+    and providing a method to calculate the area.
+
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+        size (int): The length of a side of a square.
+                    Defaults to 0 if not provided.
+
+        The direct assignment has been replaced
+        by a call to the setter to verify that
+        size is a positive integer.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Gets the size of the square.
+
+        Returns:
+            int: The side length of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square with validation.
+
+        Args:
+            value (int): The new size to set.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculates the current square area.
+
+        Returns:
+            int: The area of the square (size squared).
+        """
+        return self.__size**2
+
+    def __eq__(self, other):
+        """Magic method, tests equality based on the square area."""
+        if not isinstance(other, Square):
+            return NotImplemented
+        else:
+            return self.area() == other.area()
+
+    def __ne__(self, other):
+        """Magic method, tests equality based on the square area"""
+        if not isinstance(other, Square):
+            return NotImplemented
+        else:
+            return self.area() != other.area()
+
+    def __lt__(self, other):
+        """Magic method, tests inequality based on the square area."""
+        if not isinstance(other, Square):
+            return NotImplemented
+        else:
+            return self.area() < other.area()
+
+    def __le__(self, other):
+        """Magic method, tests inequality based on the square area."""
+        if not isinstance(other, Square):
+            return NotImplemented
+        else:
+            return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """Magic method, tests inequality based on the square area"""
+        if not isinstance(other, Square):
+            return NotImplemented
+        else:
+            return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Magic method, tests inequality based on the square area"""
+        if not isinstance(other, Square):
+            return NotImplemented
+        else:
+            return self.area() >= other.area()
